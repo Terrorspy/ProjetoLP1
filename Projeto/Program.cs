813eb90ab97f5185647770f019aecca433aa66a7
GenericExcuses <emaildotomasvaz@gmail.com>
2022-03-31 15:47:25 +0100

    Imprimir o board

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -4,9 +4,27 @@ namespace Projeto
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[5,5];
+
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    Console.Write("[ ]");   
+                    
+
+                }
+
+                Console.Write("\n");
+
+            }
+
+
+           
         }
     }
 }
