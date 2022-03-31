710f6f611efad44db493f5f8131636e93cb62557
GenericExcuses <emaildotomasvaz@gmail.com>
2022-03-31 23:01:39 +0100

    criei um mapa melhor com os valores a 0 no array e com chavetas a delimitar o mapa

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -10,21 +10,16 @@ namespace Projeto
 
             for(int i = 0; i < matrix.GetLength(0); i++)
             {
-
+                Console.Write("[ ");
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-
-                    Console.Write("[ ]");   
-                    
-
+                   Console.Write(matrix[i, j] + " ");
+                   
                 }
-
+                Console.Write("]");
                 Console.Write("\n");
-
+                
             }
-
-
-           
         }
     }
 }
